41cf4312be4bcaa58c91cd3092eea8cdc1005995
Prikhod322 <vovkaprikhod@gmail.com>
2021-07-07 12:46:50 +0300

    The numbers & arrows are dynamically sized

--- WindowsFormsApp11/CustomClock.cs
+++ WindowsFormsApp11/CustomClock.cs
@@ -58,19 +58,19 @@ namespace WindowsFormsApp11
         }
         public void DrawNumbers(Graphics g)
         {
-           
-            g.DrawString("12", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width/2-40, 20);
-            g.DrawString("1", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width / 2 + 115, 65);
-            g.DrawString("2", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width / 2 + 220, 170);
-            g.DrawString("3", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width  - 80, Height/2-40);
-            g.DrawString("4", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width - 120, Height / 2 + 90);
-            g.DrawString("5", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width - 220, Height / 2 + 200);
-            g.DrawString("6", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width /2-20, Height -100);
-            g.DrawString("7", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width / 2 -180, Height - 145);
-            g.DrawString("8", new Font("Times New Roman", 50), new SolidBrush(Color.Black), Width / 2 - 280, Height - 250);
-            g.DrawString("9", new Font("Times New Roman", 50), new SolidBrush(Color.Black), 30, Height /2-40);
-            g.DrawString("10", new Font("Times New Roman", 50), new SolidBrush(Color.Black), 60, 175);
-            g.DrawString("11", new Font("Times New Roman", 50), new SolidBrush(Color.Black), 160, 60);
+
+            g.DrawString("12", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 - (int)(Width / 17.5), Width / 35);
+            g.DrawString("1", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 + Width / 6, (int)(Width / 10.7));
+            g.DrawString("2", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 + (int)(Width / 3.1), (int)(Width / 4.1));
+            g.DrawString("3", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width - (int)(Width / 8.75), Height / 2 - (int)(Width / 17));
+            g.DrawString("4", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width - (int)(Width / 5.8), Height / 2 + (int)(Width / 7.77));
+            g.DrawString("5", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width - (int)(Width / 3.1), Height / 2 + (int)(Width / 3.5));
+            g.DrawString("6", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 - Width / 35, Height - Width / 7);
+            g.DrawString("7", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 - (int)(Width / 3.88), Height - (int)(Width / 4.8));
+            g.DrawString("8", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), Width / 2 - (int)(Width / 2.5), Height - (int)(Width / 2.8));
+            g.DrawString("9", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), (int)(Width / 23.3), Height / 2 - (int)(Width / 17.5));
+            g.DrawString("10", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), (int)(Width / 11.6), Width / 4);
+            g.DrawString("11", new Font("Times New Roman", Width / 14), new SolidBrush(Color.Black), (int)(Width / 4.37), (int)(Width / 11.6));
         }
 
 
@@ -83,16 +83,16 @@ namespace WindowsFormsApp11
            
             g.TranslateTransform(Width / 2, Height / 2);
 
-            
-            g.RotateTransform((float)RotationAngleHoursArrow);
-            g.FillRegion(new SolidBrush(ArrowColor), new Region(new Rectangle(0, 0, 13, Width / 2 - Width / 7)));
 
-            g.RotateTransform((float)RotationAngleMinutesArrow - (float)RotationAngleHoursArrow%360);
-            g.FillRegion(new SolidBrush(ArrowColor), new Region(new Rectangle(0,0,8, Width / 2 - Width / 9)));
+            g.RotateTransform((float)RotationAngleHoursArrow);
+            g.FillRegion(new SolidBrush(ArrowColor), new Region(new Rectangle(0, 0, (int)(Width / 53.8), Width / 2 - Width / 7)));
+
+            g.RotateTransform((float)RotationAngleMinutesArrow - (float)RotationAngleHoursArrow % 360);
+            g.FillRegion(new SolidBrush(ArrowColor), new Region(new Rectangle(0, 0, (int)(Width / 87.5), Width / 2 - Width / 9)));
 
 
             g.RotateTransform((float)(RotationAngleSecondsArrow));
-            g.FillRectangle(new SolidBrush(ArrowColor), new Rectangle(0,0, 5, Width/2- Width / 10));
+            g.FillRectangle(new SolidBrush(ArrowColor), new Rectangle(0, 0, Width / 140, Width / 2 - Width / 10));
 
         }
     }
